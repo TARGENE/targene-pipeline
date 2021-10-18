cc3bc3810546662b3b98212113d37a7042fa59c9
Olivier Labayle <olabayle@gmail.com>
2021-10-18 15:43:29 +0100

    add phenotypes extraction from the gene atlas

--- src/phenotypes.jl
+++ src/phenotypes.jl
@@ -1,12 +1,26 @@
 
-function columnnames(filename::String, patterns)
-    columns = []
-    for row in CSV.File(filename)
-        
+
+function from_geneatlas(parsed_args)
+    bridge = CSV.File(parsed_args["bridge"], header=["eid", "geneatlas_id"]) |> DataFrame
+    # Loading in pure datafame objects seems to be fine with 16GB of memory
+    binary_phenotypes  = CSV.File(parsed_args["binary-phenotypes"], drop=[:IID]) |> DataFrame
+    continuous_phenotypes  = CSV.File(parsed_args["continuous-phenotypes"], drop=[:IID]) |> DataFrame
+    merged = innerjoin(bridge, 
+                        binary_phenotypes, 
+                        on = :geneatlas_id => :FID)
+    merged = innerjoin(merged,
+                        continuous_phenotypes,
+                        on=:geneatlas_id=> :FID)
+    if haskey(parsed_args, "withdrawal-list")
+        withdrawn = CSV.File(parsed_args["withdrawal-list"], header=["eid"]) |> DataFrame
+        merged = filter(:eid => ∉(withdrawn.eid), merged)
     end
+
+    CSV.write(parsed_args["output"], merged)
 end
 
 function prepare_phenotypes(parsed_args)
-    patterns = parsed_args["subset"]
-    
+    if parsed_args["mode"] == "from-geneatlas"
+        from_geneatlas(parsed_args)
+    end
 end
--- test/phenotypes.jl
+++ test/phenotypes.jl
@@ -4,9 +4,33 @@ using Test
 using UKBBEpistasisPipeline
 
 
-@testset "Test columnnames" begin
-    filename = joinpath("data", "ukbb", "diseases.csv")
+@testset "Test prepare_phenotypes" begin
+    parsed_args = Dict(
+        "binary-phenotypes" => joinpath("data", "ukbb", "binary_phenotypes.csv"),
+        "continuous-phenotypes" => joinpath("data", "ukbb", "quantitative_phenotypes.csv"),
+        "bridge" => joinpath("data", "ukbb", "bridge.csv"),
+        "mode" => "from-geneatlas",
+        "withdrawal-list" => joinpath("data", "ukbb", "withdrawal_list.csv"),
+        "output" => "full_phenotypes.csv"
+    )
+    UKBBEpistasisPipeline.prepare_phenotypes(parsed_args)
     
+    phenotypes = CSV.File(parsed_args["output"]) |> DataFrame
+
+    @test names(phenotypes) == ["eid",
+                                "geneatlas_id",
+                                "clinical_c_Block_J40-J47",
+                                "clinical_c_G25",
+                                "clinical_c_G20",
+                                "1408-0.0",
+                                "1777-0.0",
+                                "1727-0.0",
+                                "1548-0.0"]
+    @test phenotypes.eid == [1000023, 1000030]
+    @test phenotypes.geneatlas_id == [1048581, 5242887]
+
+    rm(parsed_args["output"])
+
 end
 
 end;
