64b7d30d5249d51608b35d730f9b19809c87d512
Olivier Labayle <olabayle@gmail.com>
2022-01-17 19:25:42 +0100

    add grm computation

--- src/UKBBEpistasisPipeline.jl
+++ src/UKBBEpistasisPipeline.jl
@@ -5,6 +5,7 @@ using CSV
 using TOML
 using BGEN
 using SnpArrays
+using Serialization
 
 # INCLUDES
 #####################################################################
@@ -13,6 +14,7 @@ include("asb_snps.jl")
 include("queries_generation.jl")
 include("confounders.jl")
 include("phenotypes.jl")
+include("grm.jl")
 
 
 # EXPORTS
@@ -22,5 +24,6 @@ export generate_queries
 export filter_asb
 export filter_chromosome, merge_beds, adapt_flashpca
 export prepare_phenotypes
+export computeGRM
 
 end
