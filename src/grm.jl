98806563866730aafa7d9cdcca9963a01a28e8e9
Olivier Labayle <olabayle@gmail.com>
2022-01-18 10:15:44 +0100

    add GRM

--- src/grm.jl
+++ src/grm.jl
@@ -1,39 +1,35 @@
 
 using Serialization 
 
-function update(outdir, i, grmᵢ, R, finalize)
+function update(outdir, i, grmᵢ, nbSNPs, finalize)
     filepath = joinpath(outdir, "grm_$i.jls")
     if isfile(filepath)
         old_grmᵢ = deserialize(filepath)
         grmᵢ = grmᵢ + old_grmᵢ
     end
-    grmᵢ = finalize ? grmᵢ / (R-1) : grmᵢ
+    grmᵢ = finalize ? grmᵢ / (2*(nbSNPs-1)) : grmᵢ
     serialize(filepath, grmᵢ)
 end
 
 
 function computeGRM(outdir, bedfiles)
     nfiles = length(bedfiles)
-    R = 0
+    nbSNPs = 0
     mkdir(outdir)
     for (fileid, bedfile) in enumerate(bedfiles)
         snparray = SnpArray(bedfile)
         n, p = size(snparray)
-
         # Update total amount of SNPs for normalization
-        R += p
+        nbSNPs += p
 
-        # Compute centered genotype counts
-        allelecounts = counts(snparray, dims=1)
-        a2freqs = (2allelecounts[end, :] + allelecounts[end-1, :]) / 2n
-        a2copies = convert(Matrix{Float32}, snparray) .- 2repeat(transpose(a2freqs), n)
-        replace!(a2copies, NaN => 0)
-        den = 2a2freqs.*(1 .- a2freqs)
+        # Compute centered/normalized genotype counts
+        G = Matrix{Float32}(undef, n, p)
+        Base.copyto!(G, snparray, model=ADDITIVE_MODEL, impute=true, center=true, scale=true)
         
         # Update individual GRMᵢ in multithreading mode
-        for i in 1:n
-            grmᵢ = a2copies*(a2copies[i, :] ./ den)
-            update(outdir, i, grmᵢ, R, fileid==nfiles)
+        Threads.@threads for i in 1:n
+            grmᵢ = G*G[i, :]
+            update(outdir, i, grmᵢ, nbSNPs, fileid==nfiles)
         end
     end
 end
--- test/grm.jl
+++ test/grm.jl
@@ -3,24 +3,26 @@ module TestGRM
 using Test
 using UKBBEpistasisPipeline
 using SnpArrays
+using Serialization
 
 @testset "Test computeGRM" begin
     outdir = "grmmatrix"
-    bedfiles = [SnpArrays.datadir("mouse.bed")]
+    # As I use multiple times the same file, the GRM constructed from those files should 
+    # be the same as the GRM computed from only 1 file
+    bedfiles = [SnpArrays.datadir("mouse.bed"), SnpArrays.datadir("mouse.bed")]
     snparray = SnpArray(SnpArrays.datadir("mouse.bed"))
-    # Taken from the source of the package
-    m, n = size(snparray)
-    G = Matrix{Float32}(undef, m, n)
-    Base.copyto!(G, snparray, model=ADDITIVE_MODEL, impute=true, center=true, scale=true)
-    Φ = G * transpose(G)
-    Φ ./= 2n
 
+    # Taken from the source of the package but this uses matrix multiplication
     basegrm = grm(snparray)
 
     computeGRM(outdir, bedfiles)
-
-    grm₁ = deserialize(joinpath(outdir, "grm_1.jls"))
-    grm₁/2
+    # Check proper amount of lines are written
+    @test length(readdir(outdir)) == 1940
+    # Check a few lines at random
+    for i in (1, 20, 365, 1856)
+        grm_line = deserialize(joinpath(outdir, "grm_$i.jls"))
+        @test grm_line ≈ basegrm[i, :]
+    end
     rm(outdir, recursive=true)
 end
 
