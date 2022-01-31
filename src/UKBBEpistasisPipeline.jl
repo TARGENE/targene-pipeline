d0072eedbcff744ac2f721347469de732dc801da
Olivier Labayle <olabayle@gmail.com>
2022-01-31 18:13:34 +0000

    fix bugs

--- src/UKBBEpistasisPipeline.jl
+++ src/UKBBEpistasisPipeline.jl
@@ -14,7 +14,6 @@ include("asb_snps.jl")
 include("queries_generation.jl")
 include("confounders.jl")
 include("phenotypes.jl")
-include("grm.jl")
 
 
 # EXPORTS
@@ -24,6 +23,5 @@ export generate_queries
 export filter_asb
 export filter_chromosome, merge_beds, adapt_flashpca
 export prepare_phenotypes
-export computeGRM
 
 end
