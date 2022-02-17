862887abd468c7cee015c2537de0279ac4843978
Olivier Labayle <olabayle@gmail.com>
2022-02-17 10:06:32 +0000

    add variances computation

--- src/sieve_plateau.jl
+++ src/sieve_plateau.jl
@@ -9,9 +9,8 @@ function align_ic(ic, sample_ids, grm_ids)
     return aligned_ic
 end
 
-function bit_distance(grm_chunk_file, nτs)
-    grmpart = grm_part(grm_chunk_file)
-    distances = 1 .-  max.(min.(grmpart, 1), -1)
+function bit_distances(sample_grm, nτs)
+    distances = 1 .-  max.(min.(sample_grm, 1), -1)
     τs = reshape([2/i for i in 1:nτs], 1, nτs)
     return distances .<= τs
 end
@@ -61,68 +60,37 @@ end
 Each influence_curve vector is associated with a phenotype query.
 This function compute the variance estimates at each τ for all queries.
 """
-function compute_variances(influence_curves, nτs, grmfiles)
-    n_phenotypes = size(influence_curves, 2)
-    n_queries = size(influence_curves, 3)
+function compute_variances(influence_curves, grm, nτs)
+    n_samples, n_phenotypes, n_queries = size(influence_curves)
     variances = zeros(Float32, n_phenotypes, n_queries, nτs)
-    i_j_generator = generate_indices(grm_chunk_sizes(grmfiles), size(influence_curves, 1))
-    first_i, first_j = 1, 1
-    for grm_chunk_file in grmfiles
-        last_i, last_j = take!(i_j_generator)
-        chunk_distance = bit_distance(grm_chunk_file, nτs)
+    for sample_idx in 1:n_samples
+        bit_distances_ = bit_distances(grm[:, sample_idx], nτs)
         for phenotype_idx in 1:n_phenotypes
             for query_idx in 1:n_queries
                 inf_curve = influence_curves[:, phenotype_idx, query_idx]
-                inf_pp = chunk_pairwise_inf_curve(inf_curve, first_i, first_j, last_i, last_j)
-                variances[phenotype_idx, query_idx, :] .+= sum(chunk_distance .* inf_pp, dims=1)[1,:]
+                variances[phenotype_idx, query_idx, :] .+= sum(inf_curve[sample_idx] .* bit_distances_ .* inf_curve, dims=1)[1, :]
             end
-        end
-        if last_i == last_j
-            first_i, first_j = last_i + 1, 1
-        else
-            first_i, first_j = last_i, last_j + 1
         end
     end
     return variances
 end
 
-
-pairwise_product(inf_curve, row; row_init=1, row_end=row) =
-    inf_curve[row] .* inf_curve[row_init:row_end]
-
-function chunk_pairwise_inf_curve(inf_curve, first_i, first_j, last_i, last_j)
-    if first_j != 1
-        pairwise_ic = pairwise_product(inf_curve, first_i, row_init=first_j)
-        first_i += 1
-        first_j = 1
-    else
-        pairwise_ic = Float32[]
+function update_results_file!(results_file, phenotypes, variances)
+    for (phenotype_idx, phenotype) in enumerate(phenotypes)
+        results_file[phenotype]["variances"] = variances[phenotype_idx, :, :]
     end
-
-    for i in first_i:last_i-1
-        pairwise_ic = vcat(pairwise_ic, pairwise_product(inf_curve, i))
-    end
-
-    (last_i == last_j) && return vcat(pairwise_ic, pairwise_product(inf_curve, last_i))
-
-    return vcat(pairwise_ic, pairwise_product(inf_curve, last_i; row_end=last_j))
 end
 
 
-function generate_indices(sizes, n_samples)
-    Channel{Tuple{Int, Int}}() do chnl
-        sizes_ = copy(sizes)
-        grm_chunk_size = popfirst!(sizes_)
-        nb_elements = 0
-        for i in 1:n_samples
-            for j in 1:i
-                nb_elements += 1
-                if nb_elements == grm_chunk_size
-                    put!(chnl, (i, j))
-                    grm_chunk_size = popfirst!(sizes_)
-                    nb_elements = 0
-                end
-            end
-        end
-    end
+function sieve_variance_plateau(parsed_args)
+    results_file = jldopen(parsed_args["results"], "a+")
+
+    nτs = parsed_args["nb-estimators"]
+    grm, grm_ids = readGRM(parsed_args["grm-prefix"])
+    phenotypes, inf_curves = build_influence_curves(results_file, grm_ids)
+    variances = compute_variances(inf_curves, grm, nτs)
+
+    update_results_file!(results_file, phenotypes, variances)
+
+    close(results_file)
 end
