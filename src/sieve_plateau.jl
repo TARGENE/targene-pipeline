1670e2e20c12cffb727475da5cd4ecca410abb18
Olivier Labayle <olabayle@gmail.com>
2022-03-03 10:33:40 +0100

    try optimize sieve

--- src/sieve_plateau.jl
+++ src/sieve_plateau.jl
@@ -9,22 +9,33 @@ function align_ic(ic, sample_ids, grm_ids)
     return aligned_ic
 end
 
-function bit_distances(sample_grm, nτs)
-    distances = 1 .-  max.(min.(sample_grm, 1), -1)
-    τs = reshape(default_τs(nτs), 1, nτs)
-    return distances .<= τs
+"""
+    bit_distances(sample_grm, nτs)
+
+Returns a matrix of shape (n_samples,nτs) where n_samples is the 
+size of sample_grm.
+The sample_grm comes from the gcta software which is in fact GRM/2. 
+The process is thus as follows:
+- Multiply by 2 to get back to the GRM
+- Round between -1 and 1 as some elements may be beyond that limit
+- Take 1 - this value to covnert this quantity to a distance
+- For each τ return if the distance between individuals is less or equal than τ
+"""
+function bit_distances(sample_grm, τs)
+    distances = 1 .-  max.(min.(2sample_grm, 1), -1)
+    return convert(Matrix{Float32}, distances .<= τs)
 end
 
-default_τs(nτs) = [2/i for i in 1:nτs]
+default_τs(nτs) = reshape([2/i for i in 1:nτs], 1, nτs)
 
 """
     build_influence_curves(results_file, grm_ids)
 
 Load and build an influence curves 3-dimensional array with sizes:
     - N samples 
-    - N phenotypes 
     - N queries
-
+    - N phenotypes 
+    
 The ordering of the samples in the built influence_curves 
 matrix will match the GRM sample_ids.
 """
@@ -32,25 +43,51 @@ function build_influence_curves(results_file, grm_ids)
     phenotypes = keys(results_file)
     any_phenotype = first(phenotypes)
     any_queryreports = results_file[any_phenotype]["queryreports"]
+    n_observations = zeros(Int, length(phenotypes))
     influence_curves = zeros(Float32,
         size(grm_ids, 1),
+        size(any_queryreports, 1),
         size(phenotypes, 1),
-        size(any_queryreports, 1)
         )
-    fill_influence_curves!(influence_curves, results_file, phenotypes, grm_ids)
-    return phenotypes, influence_curves
+    fill_influence_curves!(influence_curves, n_observations, results_file, phenotypes, grm_ids)
+    return phenotypes, influence_curves, n_observations
 end
 
 
-function fill_influence_curves!(influence_curves, results_file, phenotypes, grm_ids)
+function fill_influence_curves!(influence_curves, n_observations, results_file, phenotypes, grm_ids)
     for (phenotype_id, phenotype) in enumerate(phenotypes)
         phenotype_group = results_file[phenotype]
         sample_ids = phenotype_group["sample_ids"]
+        n_observations[phenotype_id] = size(sample_ids, 1)
         queryreports = results_file[phenotype]["queryreports"]
         for (query_id, queryreport) in enumerate(queryreports)
             inf_curve = align_ic(queryreport.influence_curve, sample_ids, grm_ids)
-            influence_curves[:, phenotype_id, query_id] = coalesce.(inf_curve, 0)
+            influence_curves[:, query_id, phenotype_id] = coalesce.(inf_curve, 0)
         end
+    end
+end
+
+"""
+    aggregate_variance(D, indicator, i)
+
+This function computes the sum for a single index i, see also `compute_variances`.
+As the GRM is symetric it is performed as : 
+    2 times off-diagonal elements with j < i + diagonal term 
+and this for all τs.
+"""
+aggregate_variance(D, indicator) =
+    @views D[end].*(2sum(indicator[1:end-1, :] .* D[1:end-1], dims=1)[1, :] .+ D[end] .* indicator[end, :])
+
+"""
+    normalize(variances, n_observations)
+
+Divides the variance estimates by the effective number of observations 
+used for each phenotype at estimation time.
+"""
+function normalize!(variances, n_observations)
+    n_phenotypes = size(variances, 3)
+    for i in 1:n_phenotypes
+        variances[:, :, i] ./= n_observations[i]
     end
 end
 
@@ -58,21 +95,43 @@ end
 """
     compute_variances(influence_curves, nτs, grm_files)
 
-Each influence_curve vector is associated with a phenotype query.
-This function compute the variance estimates at each τ for all queries.
+An overall variance estimate for a distance function d, a threshold τ 
+and influence curve D is given by:
+            σ̂ = 1/n ∑ᵢ∑ⱼ1(dᵢⱼ ≤ τ)DᵢDⱼ
+              = 1/n 2∑ᵢDᵢ∑ⱼ<ᵢ1(dᵢⱼ ≤ τ)DᵢDⱼ + 1(dᵢᵢ ≤ τ)DᵢDᵢ
+
+This function computes those variance estimates at each τ for all phenotypes
+and queries.
+
+# Arguments:
+- influence_curves: Array of size (n_samples, n_queries, n_phenotypes)
+- grm: Vector containing the lower elements of the GRM ie: n(n+1)/2 elements
+- τs: 1 row matrix containing the distance thresholds between individuals
+- n_obs: Vector of size (n_phenotypes,), containing the number of effective 
+observations used during estimation
+
+# Returns:
+- variances: An Array of size (nτs, n_queries, n_phenotypes)
 """
-function compute_variances(influence_curves, grm, nτs)
-    n_samples, n_phenotypes, n_queries = size(influence_curves)
-    variances = zeros(Float32, n_phenotypes, n_queries, nτs)
-    for sample_idx in 1:n_samples
-        bit_distances_ = bit_distances(grm[:, sample_idx], nτs)
-        for phenotype_idx in 1:n_phenotypes
-            for query_idx in 1:n_queries
-                inf_curve = influence_curves[:, phenotype_idx, query_idx]
-                variances[phenotype_idx, query_idx, :] .+= sum(inf_curve[sample_idx] .* bit_distances_ .* inf_curve, dims=1)[1, :]
+function compute_variances(influence_curves, grm, τs, n_obs)
+    n_samples, n_queries, n_phenotypes = size(influence_curves)
+    variances = zeros(Float32, length(τs), n_queries, n_phenotypes)
+    start_idx = 1
+    @inbounds for sample in 1:n_samples
+        # lower diagonal of the GRM are stored in a single vector 
+        # that are accessed one row at a time
+        end_idx = start_idx + sample - 1
+        sample_grm = view(grm, start_idx:end_idx)
+        indicator = UKBBEpistasisPipeline.bit_distances(sample_grm, τs)
+        @sync for query_idx in 1:n_queries
+            for phenotype_idx in 1:n_phenotypes
+                @spawn variances[:, query_idx, phenotype_idx] .+= 
+                    UKBBEpistasisPipeline.aggregate_variance(view(influence_curves, 1:sample, query_idx, phenotype_idx), indicator)
             end
         end
+        start_idx = end_idx + 1
     end
+    normalize!(variances, n_obs)
     return variances
 end
 
@@ -111,13 +170,14 @@ end
 function sieve_variance_plateau(parsed_args)
     results_file = jldopen(parsed_args["results"], "a+")
 
-    nτs = parsed_args["nb-estimators"]
+    τs = default_τs(parsed_args["nb-estimators"])
     grm, grm_ids = readGRM(parsed_args["grm-prefix"])
-    phenotypes, inf_curves = build_influence_curves(results_file, grm_ids)
-    variances = compute_variances(inf_curves, grm, nτs)
+    phenotypes, inf_curves, n_obs = build_influence_curves(results_file, grm_ids)
+    variances = compute_variances(inf_curves, grm, τs, n_obs)
     #monotone_variances, radial_variances = make_monotone_and_smooth(variances)
 
     update_results_file!(results_file, phenotypes, variances)
 
     close(results_file)
 end
+
--- test/sieve_plateau.jl
+++ test/sieve_plateau.jl
@@ -32,24 +32,26 @@ end
     expected_inf_curve(queryreports, index, nbzeros) = 
         convert(Vector{Float32}, vcat(queryreports[index].influence_curve, zeros(nbzeros)))
     
-    grm_ids = UKBBEpistasisPipeline.load_grm_ids("data/grm/GRM.ids.csv")
+    grm_ids = UKBBEpistasisPipeline.GRMIDs(joinpath("data", "grm", "test.grm.id"))
 
     build_result_file(grm_ids)
 
     results_file = jldopen("results.hdf5", "a+")
 
-    phenotypes, inf_curves = UKBBEpistasisPipeline.build_influence_curves(results_file, grm_ids)
+    phenotypes, inf_curves, n_observations = UKBBEpistasisPipeline.build_influence_curves(results_file, grm_ids)
     @test phenotypes == ["cancer", "bmi"]
     @test size(inf_curves) == (size(grm_ids, 1), 2, 2)
+    @test n_observations == [184, 174]
+
 
     # The operation is converting to Float32
     # All missing elements are at the enf of the grm ids
     qrs = results_file["cancer"]["queryreports"]
     @test expected_inf_curve(qrs, 1, 10) == inf_curves[:, 1, 1]
-    @test expected_inf_curve(qrs, 2, 10) == inf_curves[:, 1, 2]
+    @test expected_inf_curve(qrs, 2, 10) == inf_curves[:, 2, 1]
 
     qrs = results_file["bmi"]["queryreports"]
-    @test expected_inf_curve(qrs, 1, 20) == inf_curves[:, 2, 1]
+    @test expected_inf_curve(qrs, 1, 20) == inf_curves[:, 1, 2]
     @test expected_inf_curve(qrs, 2, 20) == inf_curves[:, 2, 2]
 
     close(results_file)
@@ -58,55 +60,62 @@ end
 
 
 @testset "Test bit_distance" begin
-    grm_sample = Float32[-1.1, -0.8, -0.5, -0.3, -0.1, 0.1, 0.3, 0.5, 0.8, 1.1]
+    grm_sample = Float32[-0.6, -0.4, -0.25, -0.3, -0.1, 0.1, 0.3, 0.5, 0.2, 0.6]
     nτs = 5
-    # τs = reshape([2/i for i in 1:nτs], 1, nτs)
-    d = UKBBEpistasisPipeline.bit_distances(grm_sample, nτs)
+    τs = UKBBEpistasisPipeline.default_τs(nτs)
+    @test τs ≈ Float32[2.0  1.0  0.666667  0.5  0.4]
+    d = UKBBEpistasisPipeline.bit_distances(grm_sample, τs)
     @test d == [1  0  0  0  0
                 1  0  0  0  0
                 1  0  0  0  0
                 1  0  0  0  0
                 1  0  0  0  0
                 1  1  0  0  0
-                1  1  0  0  0
-                1  1  1  1  0
                 1  1  1  1  1
+                1  1  1  1  1
+                1  1  1  0  0
                 1  1  1  1  1]
 end
 
 
 @testset "Test compute_variances" begin
-    # Compute variances
-    grm, grm_ids = UKBBEpistasisPipeline.readGRM("data/grm/GRM")
-    n_phenotypes = 2
+    n_phenotypes = 3
     n_queries = 2
-    n_samples = size(grm_ids, 1)
-    influence_curves = reshape(collect(1:n_phenotypes*n_samples*n_queries), 
-                        n_samples, 
-                        n_phenotypes, 
-                        n_queries)
-    influence_curves = convert(Array{Float32, 3}, influence_curves)                 
-    nτs = 5
-    variances = UKBBEpistasisPipeline.compute_variances(influence_curves, grm, nτs)
-    @test size(variances) == (n_phenotypes, n_queries, nτs)
+    n_samples = 5
+    nτs = 4
+    n_obs = [3, 5, 4]
+    τs = UKBBEpistasisPipeline.default_τs(nτs)
+    # The GRM has 15 lower triangular elements
+    grm = [0.4, 0.1, 0.5, 0.2, -0.2, 0.6, 0.3, -0.6, 
+            0.4, 0.3, 0.6, 0.3, 0.7, 0.3, 0.1]
+    influence_curves = zeros(Float32, n_samples, n_queries, n_phenotypes)
+    influence_curves[:, 1, 1] = [0.1, 0., 0.1, 0.3, 0.]
+    influence_curves[:, 1, 2] = [0.1, 0.2, 0.1, 0.3, 0.2]
+    influence_curves[:, 1, 3] = [0.1, 0., 0.1, 0.3, 0.2]
+    influence_curves[:, 2, 1] = [0.1, 0., 0.1, 0.3, 0.]
+    influence_curves[:, 2, 2] = [0.1, 0.2, 0.1, 0.3, 0.2]
+    influence_curves[:, 2, 3] = [0.1, 0., 0.1, 0.3, 0.2]
+                  
+    
+    @enter variances = UKBBEpistasisPipeline.compute_variances(influence_curves, grm, τs, n_obs)
+    @test size(variances) == (nτs, n_queries, n_phenotypes)
 
-    # For the first τ (max distance==2), all elements are taken into account
+    # when τ=2, all elements are used
     for phenotype_idx in 1:n_phenotypes
-        for query_idx in 1:n_queries
-            inf_phen_1 = influence_curves[:, phenotype_idx, query_idx]  
-            @test variances[phenotype_idx, query_idx, 1] ≈ 
-                sum(inf_phen_1[i]*inf_phen_1[j] 
-                        for i in 1:n_samples for j in 1:n_samples)
-
+        for query_idx in n_queries
+            s = sum(influence_curves[:, query_idx, phenotype_idx])
+            var = sum(s*influence_curves[i, query_idx, phenotype_idx] for i in 1:n_samples)/n_obs[phenotype_idx]
+            @test variances[1, query_idx, phenotype_idx] ≈ var
         end
     end
 
     # Decreasing variances with τ as all inf curves are positives
     for nτ in 2:nτs
-        @test all(variances[:, :, nτ] .<= variances[:, :, nτ-1])
+        @test all(variances[nτ, :, :] .<= variances[nτ-1, :, :])
     end
 
 end
+
 
 @testset "Test sieve_variance_plateau" begin
 
