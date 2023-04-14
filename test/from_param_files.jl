13ab580824165bac22d213e849be01aa0f1d14d3
Olivier Labayle <s2042526>
2023-04-14 09:29:47 +0100

    try fixownership

--- test/from_param_files.jl
+++ test/from_param_files.jl
@@ -1,12 +1,15 @@
-args = length(ARGS) > 0 ? ARGS : ["-profile", "eddie", "-resume"] 
+# "local" profile assumes singularity is installed
+args = length(ARGS) > 0 ? ARGS : ["-profile", "local", "-resume"] 
 
 include("utils.jl")
 
 @testset "Test from_param_files.config" begin
     cmd = `nextflow run main.nf -c conf/ci_jobs/from_param_files.config $args`
     @info string("The following command will be run:\n", cmd)
+
     r = run(cmd)
     @test r.exitcode == 0
+    
     output = CSV.read(joinpath("results", "summary.csv"), DataFrame)
     dataset = CSV.read(joinpath("results", "tmle_inputs", "final.data.csv"), DataFrame)
     @test names(output) == SUMMARY_COLUMNS
