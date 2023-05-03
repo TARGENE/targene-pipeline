132f17cbf91bb46870da11e675b0c4a2a3256e6e
Olivier Labayle <s2042526>
2023-05-03 14:10:30 +0100

    add neg control pipeline and test

--- test/from_actors.jl
+++ test/from_actors.jl
@@ -15,7 +15,7 @@ include("utils.jl")
     dataset = CSV.read(joinpath("results", "tmle_inputs", "final.data.csv"), DataFrame)
     bQTLs = Symbol.(CSV.read(joinpath("test", "data", "actors", "bqtls.csv"), DataFrame).ID)
 
-    @test names(output) == vcat(SUMMARY_COLUMNS, SIEVE_COLUMNS)
+    @test names(output) == vcat(SUMMARY_COLUMNS, SIEVE_COLUMNS, ADJUTMENT_COL)
     # 2 bQTLs and 1 trans-actor
     @test Set(unique(output.TREATMENTS)) == Set(["1:238411180:T:C", "3:3502414:T:C", "1:238411180:T:C_&_2:14983:G:A", "3:3502414:T:C_&_2:14983:G:A"])
     
@@ -35,4 +35,36 @@ include("utils.jl")
         @test keys(Ψ.treatment)[1] ∈ bQTLs
     end
 
+end
+
+@testset "Test negative controls" begin
+    cmd = `nextflow run main.nf -entry negativeControl -c conf/ci_jobs/from_actors.config $args`
+    @info string("The following command will be run:\n", cmd)
+
+    r = run(cmd)
+    @test r.exitcode == 0
+
+    # Check permutation test
+    data = DataFrame(Arrow.Table(joinpath("results", "permutation_data", "permutation_dataset.arrow")))
+    
+    n_permuted_cols = 0
+    for colname in names(data)
+        if endswith(colname, "permuted")
+            n_permuted_cols +=1
+        end
+    end
+    @test n_permuted_cols > 20
+
+    parameters = deserialize(joinpath("results", "permutation_data", "permutation_param_1.bin"))
+    @test size(parameters, 1) == 100
+    @test all(Ψ isa IATE for Ψ in parameters)
+    
+    results = CSV.read(joinpath("results", "permutation_summary.csv"), DataFrame)
+    @test size(results) == (100, 20)
+    @test length(collect(skipmissing(results.TMLE_PVALUE))) > 20
+
+    # Check random variants data
+    parameters = deserialize(joinpath("results", "random_variants_parameters", "random_variants_param_1.bin"))
+    @test size(parameters, 1) > 200
+    @test all(Ψ isa IATE for Ψ in parameters)
 end
