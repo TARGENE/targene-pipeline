fa6c917a887893b4d99c54594880ce469ba08480
Olivier Labayle <s2042526>
2024-07-25 15:50:05 +0100

    up interaction config

--- test/utils.jl
+++ test/utils.jl
@@ -1,4 +1,53 @@
 
+const TRAITS = [
+  "J40-J47 Chronic lower respiratory diseases",
+  "G25 Other extrapyramidal and movement disorders",
+  "G20 Parkinson's disease",
+  "L72 Follicular cysts of skin and subcutaneous tissue",
+  "J34 Other disorders of nose and nasal sinuses",
+  "H15-H22 Disorders of sclera, cornea, iris and ciliary body",
+  "O26 Maternal care for other conditions predominantly related to pregnancy",
+  "O20 Haemorrhage in early pregnancy",
+  "O94-O99 Other obstetric conditions, not elsewhere classified",
+  "O20-O29 Other maternal disorders predominantly related to pregnancy",
+  "oesophageal disorder",
+  "bacterial infection",
+  "psychological/psychiatric problem",
+  "heart valve problem/heart murmur",
+  "other fractures",
+  "glaucoma",
+  "gastric/stomach ulcers",
+  "mumps / epidemic parotitis",
+  "other renal/kidney problem",
+  "hypothyroidism/myxoedema",
+  "C43 Malignant melanoma of skin",
+  "D37-D48 Neoplasms of uncertain or unknown behaviour",
+  "D41 Neoplasm of uncertain or unknown behaviour of urinary organs",
+  "C34 Malignant neoplasm of bronchus and lung",
+  "D06 Carcinoma in situ of cervix uteri",
+  "D05 Carcinoma in situ of breast",
+  "D04 Carcinoma in situ of skin",
+  "D03 Melanoma in situ",
+  "C50-C50 Malignant neoplasm of breast",
+  "C81-C96 Malignant neoplasms, stated or presumed to be primary, of lymphoid, haematopoietic and related tissue",
+  "Cheese intake",
+  "Part of a multiple birth",
+  "Ease of skin tanning",
+  "Variation in diet",
+  "Number of vehicles in household",
+  "Skin colour",
+  "Pork intake"
+]
+
+const PCS = [
+  "PC1",
+  "PC2",
+  "PC3",
+  "PC4",
+  "PC5",
+  "PC6"
+]
+
 """
     check_fails_are_extremely_rare_traits(output, dataset)
 
