06280f84d228c6440addcab7f940fb8693669463
Olivier Labayle <s2042526>
2024-07-25 16:11:48 +0100

    add flat test run config

--- test/custom_cohort_flat_config.jl
+++ test/custom_cohort_flat_config.jl
@@ -7,7 +7,7 @@ using DataFrames
 args = length(ARGS) > 0 ? ARGS : ["-profile", "local", "-resume"] 
 
 @testset "Test custom_cohort_flat.config" begin
-    cmd = `nextflow run main.nf -entry TARGENE -c test/configs/custom_cohort_flat.config $args`
+    cmd = `nextflow run main.nf -c test/configs/custom_cohort_flat.config $args`
     @info string("The following command will be run:\n", cmd)
 
     r = run(cmd)
