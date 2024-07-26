9b2343ec557aee5f239778d73f08a3d52f3edeb4
Olivier Labayle <s2042526>
2024-07-26 12:11:16 +0100

    add flat config run

--- test/custom_cohort_flat_config.jl
+++ test/custom_cohort_flat_config.jl
@@ -3,6 +3,9 @@ module TestCustomCohortFlatConfig
 using Test
 using Arrow
 using DataFrames
+using JLD2
+using TargetedEstimation
+using CSV
 
 args = length(ARGS) > 0 ? ARGS : ["-profile", "local", "-resume"] 
 
@@ -12,6 +15,16 @@ args = length(ARGS) > 0 ? ARGS : ["-profile", "local", "-resume"]
 
     r = run(cmd)
     @test r.exitcode == 0
+
+    dataset_origin = CSV.read(joinpath("test", "assets", "traits.csv"), DataFrame)
+    dataset = DataFrame(Arrow.Table(joinpath("results", "dataset.arrow")))
+    @test issubset(names(dataset_origin), names(dataset))
+    
+    results = jldopen(io -> io["results"], "results/results.hdf5")
+    @test length(results) > 100
+
+    svp_results = jldopen(io -> io["results"], "results/svp.hdf5")
+    @test length(svp_results) > 100
 
     # Check properly resumed
     resume_time = @elapsed run(cmd)
