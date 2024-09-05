8a6f950af3129784fe5c12b4b4f88669c29f12ee
Olivier Labayle <s2042526>
2024-09-05 15:03:59 +0100

    fix another length problem

--- test/ukb_estimands_file.jl
+++ test/ukb_estimands_file.jl
@@ -31,7 +31,7 @@ include("utils.jl")
     ## This does not affect the OSE
     @test isempty(failed_results.OSE_GLM_GLM)
     ## Less than 1/3 affected: this is still quite significant
-    @test length(failed_results.TMLE_GLM_GLM) / length(results) < 1/3
+    @test size(failed_results.TMLE_GLM_GLM, 1) / size(results, 1) < 1/3
     @test all(startswith(x.msg, "Could not fluctuate") for x ∈ failed_results.TMLE_GLM_GLM)
     dataset = DataFrame(Arrow.Table(joinpath("results", "datasets", "all_genotypes.data.arrow")))
     check_fails_are_extremely_rare_traits(failed_results.TMLE_GLM_GLM, dataset; ncases=3)
