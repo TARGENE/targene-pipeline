9ccbf9748b4a0f644e52ba5d11bedaa0088ddb00
Olivier Labayle <olabayle@gmail.com>
2026-05-12 14:46:13 +0100

    try rename lib files

---
+++ lib/LongestPrefix.groovy
@@ -0,0 +1,19 @@
+class LongestPrefix {
+    static compute(files){
+        // Only one file, strangely it is not passed as a list
+        if (files instanceof Collection == false) {
+            return files.getName()
+        }
+        // More than one file
+        def index = 0
+        while(true){
+            def current_prefix = files[0].getName()[0..index]
+            for (file in files){
+                if(file.getName()[0..index] != current_prefix){
+                    return current_prefix[0..-2]
+                }
+            }
+            index++
+        }
+    }
+}
